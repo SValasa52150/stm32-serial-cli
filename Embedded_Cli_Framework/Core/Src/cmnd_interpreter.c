a93f6b927fa5a16a58163bc80a5f0e6e34d33f4a
Sathvik Valasa <sathvikvalasa123@gmail.com>
2025-10-31 23:25:32 +0530

    Added linked list implementation to CLI
    - Implemented linked list data structure for dynamic command management  
    - Integrated list operations with CLI command handling logic  
    - Enhanced modularity and memory handling within the CLI core

--- Embedded_Cli_Framework/Core/Src/cmnd_interpreter.c
+++ Embedded_Cli_Framework/Core/Src/cmnd_interpreter.c
@@ -31,6 +31,50 @@ base_type help_command_interpreter(char *write_buffer, size_t write_buffer_len, 
 		}
 	}
 	printf("SATHVIK : List of commandss - %s%s","\n",write_buffer);
+	/* Help Command Implementation */
+	base_type help_command_interpreter(char *write_buffer, size_t write_buffer_len, const char *command_string) {
+	    const char *registered_command_string = NULL;
+	    base_type command_string_length;
+	    memset(write_buffer, 0, write_buffer_len);
+
+	    #ifdef ARRAY_BASED_COMMAND_REGISTER
+	    base_type command_index = 0;
+	    for (command_index = 0; command_index < command_count; command_index++)
+	    {
+	        if(command_index == 0) continue;
+	        registered_command_string = commands_array[command_index].command;
+	        command_string_length = strlen( registered_command_string );
+
+	        if(command_index == 1) {
+	           strncpy(write_buffer, registered_command_string, command_string_length);
+	        } else {
+	            strcat(write_buffer,"\r\n");
+	            strcat(write_buffer,registered_command_string);
+	        }
+	    }
+	    printf("SATHVIK : List of commands - %s%s","\n",write_buffer);
+
+	    #else
+	    const CLI_Definition_List_Item *command = &registered_commands;
+	    command = command->next->next;
+	    for( command ; command != NULL; command = command->next )
+	    {
+	        registered_command_string = command->command_line_definition->command;
+	        command_string_length = strlen( registered_command_string );
+
+	        if(strlen(write_buffer) == 0) {
+	        strncpy(write_buffer, registered_command_string, command_string_length);
+	        } else {
+	            strcat(write_buffer,"\r\n");
+	            strcat(write_buffer, registered_command_string);
+	        }
+	    }
+	     printf("VAMSI : List of commands - %s%s","\n",write_buffer);
+	    #endif
+
+		 // Return FALSE if no more strings are to be returned
+		 return (FALSE);
+	}
 
 	// Return FALSE if no more strings are to be returned
 	 return (FALSE);
--- Embedded_Cli_Framework/Core/Src/process_cmd.c
+++ Embedded_Cli_Framework/Core/Src/process_cmd.c
@@ -11,6 +11,8 @@ base_type process_command(const char * const command_input, char *write_buffer, 
 	base_type is_processed = PASS;
 	const char *registered_command_string = NULL;
 	base_type command_string_length;
+
+	#ifdef ARRAY_BASED_COMMAND_REGISTER
 	static int command_index = 0;
 
 	if (command_index == 0)
@@ -78,5 +80,78 @@ base_type process_command(const char * const command_input, char *write_buffer, 
 		is_processed = FALSE;
 		printf("%s\n",write_buffer);
 	}
+
+	#else /* Linked list based command registry logic from here */
+
+	static const CLI_Definition_List_Item *curr_command = NULL;
+
+	if( curr_command == NULL )
+	{
+		/* Search for the command string in the list of registered commands. */
+		for( curr_command = &registered_commands; curr_command != NULL; curr_command = curr_command->pxNext )
+		{
+			registered_command_string = curr_command->command_line_definition->command;
+			command_string_length = strlen( registered_command_string );
+
+			/* To ensure the string lengths match exactly, so as not to pick up
+			a sub-string of a longer command, check the byte after the expected
+			end of the string is either the end of the string or a space before
+			a parameter. */
+			if( ( command_input[ command_string_length ] == ' ' ) || ( command_input[ command_string_length ] == 0x00 ) )
+			{
+				if( strncmp( command_input, registered_command_string, command_string_length ) == 0 )
+				{
+					printf("VAMSI : Command received - %s\n",registered_command_string);
+					/* The command has been found.  Check it has the expected
+					number of parameters.  If expected_number_of_parameters is -1,
+					then there could be a variable number of parameters and no
+					check is made. */
+					if( curr_command->command_line_definition->expected_number_of_parameters >= 0 )
+					{
+						if( prvGetNumberOfParameters( command_input ) != curr_command->command_line_definition->expected_number_of_parameters )
+						{
+							is_processed = FALSE;
+						} else {
+							printf("VAMSI : Number of parameters are - %d\n",curr_command->command_line_definition->expected_number_of_parameters);
+						}
+					}
+					break;
+				}
+			}
+		}
+	}
+
+	if( ( curr_command != NULL ) && ( is_processed == FALSE ) )
+	{
+		/* The command was found, but the number of parameters with the command
+		was incorrect. */
+		strncpy( write_buffer, "Incorrect command parameter(s).  Enter \"help\" to view a list of available commands.\r\n\r\n", write_buffer_len );
+		curr_command = NULL;
+		printf("%s\n",write_buffer);
+	}
+	else if( curr_command != NULL )
+	{
+		/* Call the callback function that is registered to this command. */
+		is_processed = curr_command->command_line_definition->command_interpreter( write_buffer, write_buffer_len, command_input );
+
+		/* If is_processed is FALSE, then no further strings will be returned
+		after this one, and	curr_command can be reset to NULL ready to search
+		for the next entered command. */
+		if( is_processed == FALSE )
+		{
+			curr_command = NULL;
+		}
+	}
+	else
+	{
+		/* curr_command was NULL, the command was not found. */
+		strncpy( write_buffer, "Command not recognised.  Enter 'help' to view a list of available commands.\r\n\r\n", write_buffer_len );
+		is_processed = FALSE;
+		printf("%s\n",write_buffer);
+	}
+
+	#endif
+
+return is_processed;
 
 }
